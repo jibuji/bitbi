83e506ee5fdfded5a932cb3788c735ec1a76ac22
imnotsatosh <imnotsatosh@proton.me>
2024-06-22 05:42:44 +0800

    go

--- depends/packages/randomx.mk
+++ depends/packages/randomx.mk
@@ -7,25 +7,27 @@ $(package)_sha256_hash=f3e0c7165af9fd0474f985537a8f25a3ba0daeb9bed538890fbcba35a
 
 
 define $(package)_preprocess_cmds
-	cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . 
+        cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . 
+endef
+
+define $(package)_set_vars
+        $(package)_config_opts = -DCMAKE_INSTALL_PREFIX=$($(package)_staging_prefix_dir) 
+        $(package)_config_opts_linux = -DARCH=x86_64
+        $(package)_config_opts_android = -DCMAKE_TOOLCHAIN_FILE=$(ANDROID_NDK)/build/cmake/android.toolchain.cmake  -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-$(ANDROID_API_LEVEL) 
 endef
 
 define $(package)_config_cmds
-    if [ -n "$(ANDROID_NDK_HOME)" ]; then \
-        cmake -DCMAKE_TOOLCHAIN_FILE=$(ANDROID_NDK_HOME)/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-23 .; \
-    else \
-        cmake -DARCH=native .; \
-    fi
+        cmake $($(package)_config_opts) .
 endef
 
 define $(package)_build_cmds
-	$(MAKE)
+        $(MAKE)
 endef
 
 define $(package)_stage_cmds
-	mkdir -p $($(package)_staging_prefix_dir)/include $($(package)_staging_prefix_dir)/lib &&\
-	install ./src/*.h $($(package)_staging_prefix_dir)/include &&\
-	install librandomx.a $($(package)_staging_prefix_dir)/lib
+        mkdir -p $($(package)_staging_prefix_dir)/include $($(package)_staging_prefix_dir)/lib &&\
+        install ./src/*.h $($(package)_staging_prefix_dir)/include &&\
+        install librandomx.a $($(package)_staging_prefix_dir)/lib
 endef
 
 # define $(package)_postprocess_cmds
